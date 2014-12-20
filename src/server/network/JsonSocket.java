8bfa3645f704459ea615b30b2cc6e1346e0678b7
Hadi Khodabande <h74629@gmail.com>
2014-12-20 20:34:34 +0330

    Javadoc is added.

--- src/server/network/JsonSocket.java
+++ src/server/network/JsonSocket.java
@@ -9,19 +9,69 @@ import java.io.IOException;
 import java.net.Socket;
 
 /**
- * Created by Hadi on 12/17/2014 11:53 PM.
+ * This class is a wrapper for java's <code>Socket</code> that uses json strings
+ * to transmit objects.
+ * <p>
+ * A <code>JsonSocket</code> holds a <code>Gson</code> object to convert
+ * objects to json string and vice-versa, and a <code>Socket</code> to
+ * send and receive json strings.
+ * <p>
+ * To create a <code>JsonSocket</code>, one can pass an existing socket
+ * to the constructor of this class.
+ * <p>
+ * After a <code>JsonSocket</code> was successfully created, one can send and
+ * receive objects with methods <code>send</code> and <code>get</code>.
+ * <p>
+ * Note that the other endpoint of the communication must use a
+ * <code>JsonSocket</code> to send and receive objects correctly.
+ * <p>
+ * This class is a member of {@link server.network}.
+ *
+ * @see com.google.gson.Gson
+ * @see java.net.Socket
+ *
  */
 public class JsonSocket {
 
+    /**
+     * The <code>Gson</code> object is used to convert between java and json.
+     */
     private Gson mGson;
+    /**
+     * The underlying <code>Socket</code>.
+     */
     private Socket mSocket;
+    /**
+     * Input stream of the socket.
+     */
     private DataInputStream mIn;
+    /**
+     * Output stream of the socket.
+     */
     private DataOutputStream mOut;
 
+    /**
+     * Initiates new socket with specified host and port and uses
+     * that socket to transmit json strings.
+     *
+     * @param host    the host name.
+     * @param port    the port number.
+     * @throws IOException if an I/O error occurs when creating the socket or
+     *         its input or output stream.
+     * @see java.net.Socket#Socket(java.lang.String, int)
+     * @see #JsonSocket(java.net.Socket)
+     */
     public JsonSocket(String host, int port) throws IOException {
         this(new Socket(host, port));
     }
 
+    /**
+     * Uses an existing socket to transmit json strings.
+     *
+     * @param socket    the socket object.
+     * @throws IOException if an I/O error occurs when creating the
+     *         input or output stream of the socket.
+     */
     public JsonSocket(Socket socket) throws IOException {
         mGson = new Gson();
         mSocket = socket;
@@ -29,14 +79,36 @@ public class JsonSocket {
         mOut = new DataOutputStream(mSocket.getOutputStream());
     }
 
+    /**
+     * Closes the underlying socket.
+     *
+     * @throws IOException if an I/O error occurs when closing the socket.
+     * @see java.net.Socket#close
+     * @see #isClosed
+     */
     public void close() throws IOException {
         mSocket.close();
     }
 
+    /**
+     * Returns the closed state of the socket.
+     *
+     * @return true if the socket has been closed
+     * @see java.net.Socket#isClosed
+     * @see #close
+     */
     public boolean isClosed() {
         return mSocket.isClosed();
     }
 
+    /**
+     * Converts <code>obj</code> to json string and sends it via the socket's
+     * output stream.
+     *
+     * @param obj    the object to send.
+     * @throws IOException if an I/O error occurs, e.g. when the socket is closed.
+     * @see com.google.gson.Gson#toJson(java.lang.Object)
+     */
     public void send(Object obj) throws IOException {
         String json = mGson.toJson(obj);
         byte buffer[] = json.getBytes("utf-8");
@@ -44,10 +116,28 @@ public class JsonSocket {
         mOut.write(buffer, 0, buffer.length);
     }
 
+    /**
+     * Reads a json string from socket's input stream, and converts it to
+     * a <code>JsonObject</code>.
+     *
+     * @return the received <code>JsonObject</code>
+     * @throws IOException if an I/O error occurs, e.g. when the socket is closed.
+     * @see com.google.gson.Gson#fromJson(String,java.lang.Class)
+     * @see #get(java.lang.Class)
+     */
     public JsonObject get() throws IOException {
         return get(JsonObject.class);
     }
 
+    /**
+     * Reads a json string from socket's input stream, and converts it to
+     * a an object of the specified class.
+     *
+     * @return the received object
+     * @throws IOException if an I/O error occurs, e.g. when the socket is closed.
+     * @see com.google.gson.Gson#fromJson(String,java.lang.Class)
+     * @see #get
+     */
     public <T> T get(Class<T> classOfInput) throws IOException {
         int length = mIn.readInt(), total = 0, current;
         byte buffer[] = new byte[length];
--- src/server/network/NetServerThread.java
+++ src/server/network/NetServerThread.java
@@ -5,30 +5,84 @@ import java.net.ServerSocket;
 import java.util.function.Consumer;
 
 /**
- * Created by Hadi on 12/17/2014 11:56 PM.
+ * This thread is used by {@link server.network.NetServer} to listen on
+ * some port for the new clients.
+ * <p>
+ * The actual work of the {@link server.network.NetServerThread} is done by a
+ * {@link java.net.ServerSocket}. When a <code>NetServerThread</code> is ran,
+ * it creates a new server socket to listen on that port. When a new client is
+ * connected to that port, it passes the client's
+ * {@link server.network.JsonSocket} to the specified consumer.
+ * <p>
+ * The consumer should accept the client and perform necessary operations.
+ * <p>
+ * In the {@link server.network.NetServer} the consumer is the abstract method
+ * {@link server.network.NetServer#accept}.
+ * <p>
+ * When the server socket is disconnected from the port, e.g. when it is closed
+ * due to an unknown reason, a new server socket will be created.
+ * So it is guaranteed that server is always listening on the port.
+ *
+ * @see server.network.NetServer
+ * @see java.net.ServerSocket
  */
 public class NetServerThread extends Thread {
 
+    /**
+     * The port to listen on.
+     */
     private final int port;
+    /**
+     * Terminate flag!
+     */
     private boolean terminateFlag;
+    /**
+     * The underlying server socket which actually listens on the port.
+     */
     private ServerSocket serverSocket;
+    /**
+     * This consumer specifies the behavior of the server when a new client is
+     * connected.
+     * Actually, <code>clientAcceptor.accept(JsonSocket)</code> is called when
+     * a client is connected to the port.
+     */
     private Consumer<JsonSocket> clientAcceptor;
 
+    /**
+     * Constructor.
+     * @param port              the server port
+     * @param clientAcceptor    specifies accept behavior of the server
+     */
     public NetServerThread(int port, Consumer<JsonSocket> clientAcceptor) {
         this.port = port;
         this.clientAcceptor = clientAcceptor;
     }
 
+    /**
+     * Runs the server until the <code>terminateFlag</code> is set to
+     * <code>true</code>.
+     *
+     * @see #runServer
+     * @see #terminate
+     */
     @Override
     public void run() {
         while (!terminateFlag)
             try {
                 runServer();
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
     }
 
+    /**
+     * Runs the server and handles new clients. The method can not be completed
+     * without throwing an exception.
+     *
+     * @throws IOException if an I/O error occurs,
+     *         e.g. when the server socket is closed.
+     * @see #terminate
+     */
     private void runServer() throws IOException {
         if (serverSocket != null && serverSocket.isClosed())
             serverSocket.close();
@@ -37,9 +91,17 @@ public class NetServerThread extends Thread {
             clientAcceptor.accept(new JsonSocket(serverSocket.accept()));
     }
 
-    public synchronized void terminate() throws IOException {
+    /**
+     * Terminates operations of the current thread. Sets the terminate flag
+     * to <code>true</code> and closes the server socket.
+     */
+    public synchronized void terminate() {
         terminateFlag = true;
-        serverSocket.close();
+        try {
+            serverSocket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
