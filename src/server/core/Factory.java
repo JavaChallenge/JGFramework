a95d8ff6759ec8b09045e8d48980432a5bc486a3
AliLotfi <alilotfi3@gmail.com>
2015-01-27 22:17:31 +0330

    Server implemented Factory class added

---
+++ src/server/core/Factory.java
@@ -0,0 +1,19 @@
+package server.core;
+
+/**
+ * Factory class is based on factory design pattern, and returns preferred subclass of the
+ * {@link server.core.GameLogic GameLogic} class which represents the main game logic engine of the game.
+ * <p>
+ *     The user must fill in this method to return the preferred subclass of the
+ *     {@link server.core.GameLogic GameLogic} in order to be used in the
+ *     {@link server.core.GameHandler GameHandler} class
+ * </p>
+ */
+public interface Factory {
+
+    /**
+     * The user fills in this abstract method to return the preferred subclass of the game logic.
+     * @return The preferred child of the {@link server.core.GameLogic GameLogic} class
+     */
+    public GameLogic getGameLogic();
+}
